1e37d3f3175ae2aa43023248194550a5dae6d144
Hudson Lessa <admin@hudsonlessa.com>
2022-04-05 20:07:33 -0300

    Load glsl, vs, fs, vert and frag files

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,10 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.(glsl|vs|fs|vert|frag)$/,
+        type: 'asset/source',
+      },
     ],
   },
   plugins: [
